16ed5b784e27e182b07c201e81c7f6f64e3cbffe
Ashutosh <ashutosh2001sharma@gmail.com>
2022-03-29 12:16:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+import Coins from './components/Coins'
+import Coin from './routes/Coin'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('');
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      // console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+  
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+  return (
+    <>
+      <Navbar />
+      
+      <Routes>
+        <Route path='/' element={<Coins coins={coins} />} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
+        </Route>
+      </Routes>
+      
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import './Navbar.css'
+
+const Navbar = () => {
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
+
+    return (
+        <div className='header'>
+            <div className='container'>
+                <h1>CRYPTO</h1>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li>
+                        <a href='https://cryptowebapp0024.netlify.app/'>Home</a>
+                    </li>
+                    <li>
+                        <a href='/'>Tracker</a>
+                    </li>
+                
+                </ul>
+                <div className='input-container'>
+                    <button className='btn'>Go to Wallet</button>
+                </div>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={20} style={{color: '#333'}}/>) : (<FaBars size={20} style={{color: '#333'}} />)}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
+
